be6bfe3a520a65effaf2a9b35756a2053bda714c
hasankhadra <hasankhadra2013@gmail.com>
2021-12-03 00:54:23 +0300

    Fixing bugs

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mkdir minio ; sudo chmod -R 777 minio ; mkdir pgadmin ; sudo chmod -R 777 pgadmin 
+mkdir logs ; mkdir minio ; sudo chmod -R 777 minio ; mkdir pgadmin ; sudo chmod -R 777 pgadmin  ; echo -e "AIRFLOW_UID=$(id -u)" > .env
 
